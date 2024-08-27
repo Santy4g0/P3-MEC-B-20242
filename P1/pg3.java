723aabe43e2f75dc1bb41982140ebc06dc929807
Santy4g0 <santgruiz853@gmail.com>
2024-08-27 15:43:49 -0500

    Create pg3.java

---
+++ P1/pg3.java
@@ -0,0 +1,27 @@
+
+/* crear dos metodos puros que permitan obtener area y perimetro del triangulo asumiendo que todos son triangulos rectangulos*/
+public class Main {
+    public static void main(String[] args) {
+      public class triangulo;
+      int base, altura;
+      private class triangulo;
+       int base=20,altura=50;
+       private class triangulo(int base=20, altura=50);
+       
+     
+
+    private int base;
+    private int altura;
+
+    public Triangulo(int base, int altura) {
+        this.base = base;
+        this.altura = altura;
+    }
+    public static void area(int base int altura){
+    String area = new area();
+    area=base*altura;
+}
+public static void (int base int altura){
+    String perimetro = new perimetro();
+    perimetro=base+(Math.sqrt((Math.pow(base/2)2)+((Math.pow(altura)2));
+}  
