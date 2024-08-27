12078f767edb58cdf9113bc17349de632bff022b
Santy4g0 <santgruiz853@gmail.com>
2024-08-27 15:23:48 -0500

    Update pg1.java

--- P1/pg1.java
+++ P1/pg1.java
@@ -1,1 +1,17 @@
 
+/*crear una clase llamada triangulo con atributos base y altura aplicar encapsulamiento y los metodos apropiados para inicializar o crear objetos con sus respectivos atributos
+*/
+
+public class Main {
+    public static void main(String[] args) {
+      public class triangulo 
+              ublic class Fraccion {
+
+    private int base;
+    private int altura;
+
+  
+    public Triangulo(int base, int altura) {
+        this.base = base;
+        this.altura = altura;
+    }
