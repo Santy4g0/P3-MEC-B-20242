7607b56d5fbaa22fe4f2b4e3f2cddc0e17d1269b
Santy4g0 <santgruiz853@gmail.com>
2024-08-27 15:34:50 -0500

    Update pg2.java

--- P1/pg2.java
+++ P1/pg2.java
@@ -1,1 +1,20 @@
+/* A PARTIR DE LA MISMA CLASE CREAR SOBRE CARGA AL CONSTRUCTOR Y CREAR DOS OBJETOS PARA CADA UNA DE LAS FORMAS DEL CONSTRUCTOR*/
+public class Main {
+    public static void main(String[] args) {
+      public class triangulo;
+      int base, altura;
+      private class triangulo;
+       int base=20,altura=50;
+       private class triangulo(int base=20, altura=50);
+       
+     
 
+    private int base;
+    private int altura;
+
+    public Triangulo(int base, int altura) {
+        this.base = base;
+        this.altura = altura;
+    }
+
+  
